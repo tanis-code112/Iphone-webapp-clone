41f5839280340457a349a651c5771692ecf884d6
Tanishq <tanishq.24bcs10051@sst.scaler.com>
2025-04-21 02:38:10 +0530

    fi

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      authToken: 'your-sentry-auth-token-here',  // Add your Sentry auth token here
+      org: "jsm-x9",
+      project: "javascript-react",
+    })
+  ],
+  
+  build: {
+    sourcemap: true
+  }
+})
